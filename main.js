24cce14dd2c16cb86be424d4988e92c606915744
sakurakun999 <yukisugoi114@gmail.com>
2024-03-20 20:24:36 +0900

    初期追加

---
+++ main.js
@@ -0,0 +1,40 @@
+let answer = generateNumber();
+
+function generateNumber() {
+    let num = "";
+    while(num.length < 3){
+        let newNum = Math.floor(Math.random() * 10);
+        if(!num.includes(newNum)){
+            num += newNum;
+        }
+    }
+    return num;
+}
+
+function checkNumber() {
+    let guess = document.getElementById("inputNumber").value;
+
+    if(guess.length !== 3 || isNaN(guess)){
+        document.getElementById("result").innerText = "3桁の数字を入力してください！";
+        return;
+    }
+
+    let eats = 0;
+    let bites = 0;
+
+    for(let i = 0; i < 3; i++){
+        if(guess[i] === answer[i]){
+            eats++;
+        }
+        else if(answer.includes(guess[i])){
+            bites++;
+        }
+    }
+
+    if(eats === 3){
+        document.getElementById("result").innerText = "おめでとう！相手の数字を当てました！";
+    }
+    else{
+        document.getElementById("result").innerText = eats + "イート・" + bites + "バイト";
+    }
+}
